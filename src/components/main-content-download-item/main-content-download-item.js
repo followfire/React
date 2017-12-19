9f415c43e4852c799f6edf51c67c0c3929f4c897
XZM <xuzhimeng@qq.com>
2017-12-19 18:32:14 +0800

    nav 溢出隐藏

--- src/components/main-content-download-item/main-content-download-item.js
+++ src/components/main-content-download-item/main-content-download-item.js
@@ -5,7 +5,7 @@ export default class MainContentDownloadItem extends Component{
     render(){
         return (
             <div className='main-content-download-item'>
-                <a href={this.props.url}>
+                <a href={this.props.url} title={this.props.text}>
                     <div>
                         <i>{this.props.icon}</i>
                         {this.props.text}
